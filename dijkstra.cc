7f73476239bb6362f38c0678806b58777e53a48e
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2013-07-27 22:10:16 +0200

    sdi

--- dijkstra.cc
+++ dijkstra.cc
@@ -15,8 +15,15 @@ dijkstra(const Graph &g, Vertex src, Vertex dst, int p)
   while(!q.empty())
     {
       pair<int, Vertex> e = q.top();
+      q.pop();
       int c = e.first;
       Vertex v = e.second;      
+
+      // Itereate over the out edges of the vertex.
+      graph_traits<Graph>::out_edge_iterator ei, eei;
+      for(tie(ei, eei) = out_edges(v, g); ei != eei; ++ei)
+	{
+	}
     };
 
   return r;
