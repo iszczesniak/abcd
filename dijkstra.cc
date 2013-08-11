5991df05eb65c961ee0e986ff2a4c5652f481add
Irek Szczesniak <irek.szczesniak@gmail.com>
2013-08-11 23:55:33 +0200

    sdi

--- dijkstra.cc
+++ dijkstra.cc
@@ -1,7 +1,9 @@
 #include "dijkstra.hpp"
 #include <queue>
 #include <utility>
+#include <boost/iterator/counting_iterator.hpp>
 
+using namespace boost;
 using namespace std;
 
 MSCPI
@@ -11,7 +13,14 @@ dijkstra(const Graph &g, Vertex src, Vertex dst, int p)
 
   // The null edge iterator.
   graph_traits<const Graph>::edge_iterator nei = edges(g).second;
-  
+
+  {
+    set<int> all(counting_iterator<int>(0),
+                 counting_iterator<int>(10));
+
+    r[src].insert(CPI(0, PI(*nei, all)));
+  }
+
   // The priority queue of the pairs, where the first element in the
   // pair tells the cost of reaching vertex that is the second element
   // in the pair.
