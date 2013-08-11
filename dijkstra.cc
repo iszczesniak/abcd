26700a8b754b72e9d33992206388ea6f0ad08beb
Irek Szczesniak <irek.szczesniak@gmail.com>
2013-08-11 23:02:40 +0200

    sdi

--- dijkstra.cc
+++ dijkstra.cc
@@ -1,30 +1,42 @@
 #include "dijkstra.hpp"
 #include <queue>
+#include <utility>
 
 using namespace std;
 
-VPM
+MSCPI
 dijkstra(const Graph &g, Vertex src, Vertex dst, int p)
 {
-  VPM r;
+  MSCPI r;
 
-  priority_queue<pair<int, Vertex> > q;
+  // The null edge iterator.
+  graph_traits<const Graph>::edge_iterator nei = edges(g).second;
+  
+  // The priority queue of the pairs, where the first element in the
+  // pair tells the cost of reaching vertex that is the second element
+  // in the pair.
+  priority_queue<pair<int, pair<Vertex, Edge> > > q;
 
-  q.push(make_pair(0, src));
+  // We reach the source vertex with cost 0 and along the null edge.
+  q.push(make_pair(0, make_pair(src, *nei)));
 
   while(!q.empty())
     {
       // Here we process vertex v.  
-      pair<int, Vertex> e = q.top();
+      pair<int, pair<Vertex, Edge> > pqe = q.top();
       q.pop();
-      int c = e.first;
-      Vertex v = e.second;      
+      int c = pqe.first;
+      Vertex v = pqe.second.first;
+      Edge e = pqe.second.second;
+
+      // We reached vertex v with cost c.  Now we should know with
+      // what subcarriers we reached this node.
 
       // Itereate over the out edges of the vertex.
-      graph_traits<Graph>::out_edge_iterator ei, eei;
-      for(tie(ei, eei) = out_edges(v, g); ei != eei; ++ei)
-	{
-	}
+      //      graph_traits<Graph>::out_edge_iterator ei, eei;
+      //      for(tie(ei, eei) = out_edges(v, g); ei != eei; ++ei)
+      //	{
+      //	}
     }
 
   return r;
--- dijkstra.hpp
+++ dijkstra.hpp
@@ -1,17 +1,23 @@
 #include "graph.hpp"
 
-// Band description.  The first elemet tells the starting suncarrier
-// number, the second the number of contguous subcarriers used.
-typedef pair<int, int> BD;
+#include <map>
+#include <set>
+#include <utility>
 
-// The set of band descriptions.
-typedef set<BD> BDS;
+// The set of subcarriers.
+typedef set<int> SSC;
 
-// Predecessor map.
-typedef map<BD, Edge> PM;
+// The predecessor info.
+typedef pair<Edge, SSC> PI;
+
+// The cost associated with the predecessor info.
+typedef pair<int, PI> CPI;
+
+// The set of the CPIs.
+typedef set<CPI> SCPI;
 
 // Vertex predecessor map, i.e. a map of vertexes to predecessor maps.
-typedef map<Vertex, PM> VPM;
+typedef map<Vertex, SCPI> MSCPI;
 
-VPM
+MSCPI
 dijkstra(const Graph &g, Vertex src, Vertex dst, int p);
