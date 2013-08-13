cc0d6abe3ac8b7dd338356d2521e6514b1d31d77
Irek Szczesniak <irek.szczesniak@gmail.com>
2013-08-13 20:54:12 +0200

    dijkstra

--- dijkstra.cc
+++ dijkstra.cc
@@ -14,19 +14,19 @@ dijkstra(const Graph &g, Vertex src, Vertex dst, int p, const SSC &ssc)
   // The null edge iterator.
   graph_traits<const Graph>::edge_iterator nei = edges(g).second;
 
-  {
-    set<int> all(counting_iterator<int>(0),
-                 counting_iterator<int>(10));
-
-    r[src].insert(CPI(0, PI(*nei, ssc)));
-  }
+  // We put here the information that allows us to process the source
+  // node in the loop below.  We say that we reach the source node
+  // with cost 0 on the subcarriers passed in the ssc argument along
+  // the null edge.  The null edge signals the beginning of the path.
+  r[src].insert(CPI(0, PI(*nei, ssc)));
 
   // The priority queue of the pairs, where the first element in the
   // pair tells the cost of reaching vertex that is the second element
-  // in the pair.
+  // in the pair.  We need not only the vertex, but also the edge to
+  // know what subcarriers we exactly need.
   priority_queue<pair<int, pair<Vertex, Edge> > > q;
 
-  // We reach the source vertex with cost 0 and along the null edge.
+  // We reach the source vertex with cost 0 along the null edge.
   q.push(make_pair(0, make_pair(src, *nei)));
 
   while(!q.empty())
