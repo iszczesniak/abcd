ae77cc23d311940be782e9b96027422f0b043ae4
Irek Szczesniak <irek.szczesniak@gmail.com>
2013-08-19 22:19:31 +0200

    tests

--- test/utils.cc
+++ test/utils.cc
@@ -4,7 +4,12 @@
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(exclude_test)
+BOOST_AUTO_TEST_CASE(exclude_test_1)
+{
+  BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_CASE(exclude_test_2)
 {
   BOOST_CHECK(true);
 }
