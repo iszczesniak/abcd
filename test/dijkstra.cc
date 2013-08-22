4787022ba95d7a2261f006cc4a1a86305a42ad14
Irek Szczesniak <irek.szczesniak@gmail.com>
2013-08-22 15:15:45 +0200

    dijkstra unit tests

--- test/dijkstra.cc
+++ test/dijkstra.cc
@@ -16,10 +16,60 @@ BOOST_AUTO_TEST_CASE(dijkstra_test_1)
   add_edge(src, dst, g);
   set_subcarriers(g, 2);
 
-  SSC ssc(counting_iterator<int>(0),
-          counting_iterator<int>(3));
+  SSC ssc(counting_iterator<int>(0), counting_iterator<int>(3));
 
   V2C2S result = dijkstra(g, src, dst, 3, ssc);
 
   BOOST_CHECK(result[dst].empty());
+}
+
+BOOST_AUTO_TEST_CASE(dijkstra_test_2)
+{
+  Graph g(2);
+  Vertex src = *(vertices(g).first);
+  Vertex dst = *(vertices(g).first + 1);
+  add_edge(src, dst, g);
+  set_subcarriers(g, 3);
+
+  SSC ssc(counting_iterator<int>(0), counting_iterator<int>(3));
+
+  V2C2S result = dijkstra(g, src, dst, 3, ssc);
+
+  BOOST_CHECK(!result[dst].empty());
+  BOOST_CHECK(result[dst].begin()->second.size() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(dijkstra_test_3)
+{
+  Graph g(3);
+  Vertex src = *(vertices(g).first);
+  Vertex mid = *(vertices(g).first + 1);
+  Vertex dst = *(vertices(g).first + 2);
+  Edge e1 = add_edge(src, mid, g).first;
+  Edge e2 = add_edge(src, mid, g).first;
+  Edge e3 = add_edge(mid, dst, g).first;
+
+  // Props of edge e1.
+  get(edge_weight, g, e1) = 1;
+  get(edge_subcarriers, g, e1).insert(0);
+  get(edge_subcarriers, g, e1).insert(1);
+
+  // Props of edge e2.
+  get(edge_weight, g, e2) = 2;
+  get(edge_subcarriers, g, e2).insert(2);
+  get(edge_subcarriers, g, e2).insert(3);
+
+  // Props of edge e3.
+  get(edge_weight, g, e3) = 1;
+  get(edge_subcarriers, g, e3).insert(2);
+  get(edge_subcarriers, g, e3).insert(3);
+
+  // Make sure we can represent multi-graphs.
+  BOOST_CHECK(e1 != e2);
+
+  SSC ssc(counting_iterator<int>(0), counting_iterator<int>(4));
+
+  V2C2S result = dijkstra(g, src, dst, 2, ssc);
+
+  BOOST_CHECK(!result[dst].empty());
 }
