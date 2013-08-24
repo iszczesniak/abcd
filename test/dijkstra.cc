11866e32338928d1ebfb0638224be572f6dc6af8
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2013-08-24 23:55:39 +0200

    dijkstra

--- test/dijkstra.cc
+++ test/dijkstra.cc
@@ -8,6 +8,10 @@
 
 #include <boost/test/unit_test.hpp>
 
+/*
+ * Make sure that we can't find a path if there are no subcarriers.
+ * The graph is a simple case of two nodes and a single link.
+ */
 BOOST_AUTO_TEST_CASE(dijkstra_test_1)
 {
   Graph g(2);
@@ -23,6 +27,10 @@ BOOST_AUTO_TEST_CASE(dijkstra_test_1)
   BOOST_CHECK(result[dst].empty());
 }
 
+/*
+ * Make sure that we can find a path if there are subcarriers.  The
+ * graph is a simple case of two nodes and a single link.
+ */
 BOOST_AUTO_TEST_CASE(dijkstra_test_2)
 {
   Graph g(2);
@@ -39,6 +47,10 @@ BOOST_AUTO_TEST_CASE(dijkstra_test_2)
   BOOST_CHECK(result[dst].begin()->second.size() == 3);
 }
 
+/*
+ * Make sure we can establish a path with a worse cost then initially
+ * it seemed.
+ */
 BOOST_AUTO_TEST_CASE(dijkstra_test_3)
 {
   Graph g(3);
@@ -81,4 +93,45 @@ BOOST_AUTO_TEST_CASE(dijkstra_test_3)
   BOOST_CHECK(result[dst].begin()->second.size() == 2);
   BOOST_CHECK(*(result[dst].begin()->second.begin()) == 2);
   BOOST_CHECK(*(++(result[dst].begin()->second.begin())) == 3);
+}
+
+/*
+ * Make sure we don't remember at node mid the results for edge e1
+ * that led to that node with a worse cost then edge e2 and with the
+ * same subcarriers.
+ */
+BOOST_AUTO_TEST_CASE(dijkstra_test_4)
+{
+  Graph g(3);
+  Vertex src = *(vertices(g).first);
+  Vertex mid = *(vertices(g).first + 1);
+  Vertex dst = *(vertices(g).first + 2);
+  Edge e1 = add_edge(src, mid, g).first;
+  Edge e2 = add_edge(src, mid, g).first;
+  Edge e3 = add_edge(mid, dst, g).first;
+
+  // Props of edge e1.
+  get(edge_weight, g, e1) = 2;
+  get(edge_subcarriers, g, e1).insert(0);
+
+  // Props of edge e2.
+  get(edge_weight, g, e2) = 1;
+  get(edge_subcarriers, g, e2).insert(0);
+
+  // Props of edge e3.
+  get(edge_weight, g, e3) = 2;
+  get(edge_subcarriers, g, e3).insert(0);
+
+  SSC ssc(counting_iterator<int>(0), counting_iterator<int>(1));
+
+  V2C2S result = dijkstra(g, src, dst, 1, ssc);
+
+  // We found the path.
+  BOOST_CHECK(!result[dst].empty());
+  // The cost of the path is 3.
+  BOOST_CHECK(result[dst].begin()->first.first == 3);
+  // We reach node mid by edge e2.
+  BOOST_CHECK(result[mid].begin()->first.second == e2);
+  // We don't remember the results for edge e1.
+  BOOST_CHECK(result[mid].begin()->second.size() == 1);
 }
