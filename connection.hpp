69b4718207c76bcdb3089e07bbae3682317f2d72
Irek Szcześniak <irek.szczesniak@gmail.com>
2013-11-26 09:10:38 +0100

    connection

---
+++ connection.hpp
@@ -0,0 +1,9 @@
+#ifndef CONNECTION_HPP
+#define CONNECTION_HPP
+
+#include "graph.hpp"
+
+// The type of the connection.
+typedef std::pair<demand, sscpath> connection;
+
+#endif /* CONNECTION_HPP */
