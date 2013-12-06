1a0b817c8f560f3bff464e11cb2c8bf36ee77884
Irek Szcześniak <irek.szczesniak@gmail.com>
2013-12-06 17:41:27 +0100

    a unit test for sdi_args

---
+++ test/sdi_args.cc
@@ -0,0 +1,17 @@
+#include "utils.hpp"
+#include "utils_netgen.hpp"
+#include "dijkstra.hpp"
+
+#include <iostream>
+
+#define BOOST_TEST_MODULE Arguments
+
+#include <boost/test/unit_test.hpp>
+
+/*
+ * Test the arguments are interpreted right.
+ */
+BOOST_AUTO_TEST_CASE(sdi_args_test_1)
+{
+  BOOST_CHECK(true);
+}
