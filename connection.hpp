db6b771c52861510a8db790e20aa98bd0110ecec
Irek Szcześniak <irek.szczesniak@gmail.com>
2014-01-03 14:29:13 +0100

    sdi args

--- connection.hpp
+++ connection.hpp
@@ -6,8 +6,11 @@
 // The type of the connection.
 class connection
 {
-  graph &g;
+public:
+  enum reconf_t {part, anew};
 
+private:
+  graph &g;
   demand d;
   sscpath p;
 
@@ -18,6 +21,14 @@ public:
   bool set_up(const demand &d);
   bool reconfigure(vertex new_Src);
   void tear_down();
+  static reconf_t &get_reconf();
+
+private:
+  bool reconfigure_part(vertex new_Src);  
+  bool reconfigure_anew(vertex new_Src);
+
+  // The parameter that tells how to reconfigure connections.
+  static reconf_t reconf;
 };
 
 #endif /* CONNECTION_HPP */
