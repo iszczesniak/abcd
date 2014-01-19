a75d6330bed3496567ebffff249dc023396db939
Irek Szcześniak <irek.szczesniak@gmail.com>
2014-01-19 21:53:32 +0100

    moved dijkstra to its own class

--- dijkstra.hpp
+++ dijkstra.hpp
@@ -3,6 +3,8 @@
 
 #include "graph.hpp"
 
+#include <map>
+
 class dijkstra
 {
   // first - the first subcarriers that fit the demand are chosen
@@ -11,7 +13,7 @@ class dijkstra
 
   enum select_t {first, smallest};
 
-  typedef map<CEP, vertex> pqueue;
+  typedef std::map<CEP, vertex> pqueue;
 
 public:
   /**
@@ -19,34 +21,34 @@ public:
    * search at node src.  In this version of the function we try to
    * find a path without any constraints on the ssc.
    */
-  V2C2S
-  dijkstra(const graph &g, const demand &d);
+  static V2C2S
+  search(const graph &g, const demand &d);
 
   /**
    * Find the shortest path in graph g for demand d.  We start the
    * search at node src with the set of subcarriers ssc.
    */
-  V2C2S
-  dijkstra(const graph &g, const demand &d, const SSC &ssc);
+  static V2C2S
+  search(const graph &g, const demand &d, const SSC &ssc);
 
   /**
    * Returns a candidate shortest path.
    */
-  sscpath
-  shortest_path(const graph &g, const V2C2S &r, const demand &d);
+  static sscpath
+  trace(const graph &g, const V2C2S &r, const demand &d);
 
   /**
    * Set up the path in the graph.  This process takes away the
    * subcarriers on the edges that are used by the path.
    */
-  void
+  static void
   set_up_path(graph &g, const sscpath &p);
 
   /**
    * Tear down the path in the graph.  This process puts back the
    * subcarriers on the edges that are used by the path.
    */
-  void
+  static void
   tear_down_path(graph &g, const sscpath &p);
 
 private:
@@ -55,15 +57,23 @@ private:
    * new result, i.e. of a lower or equal cost and with a SSC that
    * includes "ssc".
    */
-  bool
+  static bool
   has_better_or_equal(const C2S &c2s, const COST &cost, const SSC &ssc);
 
   /**
    * Check whether there is a worse or equal result in c2s, i.e. of a
    * larger or equal cost and with a SSC that is included in "ssc".
    */
-  void
+  static void
   purge_worse(pqueue &q, C2S &c2s, const COST &cost, const SSC &ssc);
-}
+
+  static void
+  relaks(pqueue &q, C2S &c2s, const CEP &cep, vertex v,
+         const SSC &ssc);
+
+  static void
+  relaks(pqueue &q, C2S &c2s, const CEP &cep, vertex v,
+         const SSSC &sssc);
+};
 
 #endif /* DIJKSTRA_HPP */
