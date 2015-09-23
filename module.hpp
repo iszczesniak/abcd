8177034f4dc3e28d556e683700d4b3aad925bd67
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-09-23 14:18:12 +0200

    module

--- module.hpp
+++ module.hpp
@@ -3,15 +3,23 @@
 
 #include "event.hpp"
 
+// This is the abstract class for modules.  A module processes events
+// with the operator() function.  The module is supposed to know why
+// it's called.  An event is scheduled with the schedule function.
 class module
 {
   pqueue &q;
 
 public:
+  // A module needs a priority queue for events.
   module(pqueue &);
   virtual ~module();
-  virtual void operator()(double) = 0;
-  void schedule(double);
+
+  // This function processes an event for this module.
+  virtual void operator()(double t) = 0;
+
+  // Schedule an event for this module at time t.
+  void schedule(double t);
 };
 
 #endif /* MODULE_HPP */
