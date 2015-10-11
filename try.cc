723c7c6ee4b55db42e7100f8e46869b566457a6c
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-11 09:16:34 +0200

    test program

---
+++ try.cc
@@ -0,0 +1,14 @@
+//#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+//#include <boost/graph/find_flow_cost.hpp>
+
+#include "graph.hpp"
+
+int main() {
+  vertex s, t;
+  graph g; 
+
+  // boost::successive_shortest_path_nonnegative_weights(g, s, t);
+  //  int cost =  boost::find_flow_cost(g);
+
+  return 0;
+}
