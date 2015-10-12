dd9d1af2fd78a4940499095e1ea969643033bd9b
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-12 11:43:44 +0200

    max-flow

--- try.cc
+++ try.cc
@@ -1,5 +1,5 @@
 #include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
-//#include <boost/graph/find_flow_cost.hpp>
+#include <boost/graph/find_flow_cost.hpp>
 
 #include "graph.hpp"
 
@@ -8,7 +8,7 @@ int main() {
   graph g; 
 
   boost::successive_shortest_path_nonnegative_weights(g, s, t);
-  //  int cost =  boost::find_flow_cost(g);
+  int cost =  boost::find_flow_cost(g);
 
   return 0;
 }
