9900ba7cbb4ea54e14286ce582aacd91206223ff
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-15 14:58:40 +0200

    try

--- try.cc
+++ try.cc
@@ -1,26 +1,24 @@
-#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
-#include <boost/graph/find_flow_cost.hpp>
-#include <boost/random.hpp>
+#include<iostream>
 
 #include "graph.hpp"
-#include "sdi_args.hpp"
-#include "utils_netgen.hpp"
 
 int main() {
-  sdi_args args;
+  graph g0;
+  graph &g1 = g0.create_subgraph();
 
-  args.network = random_network;
-  args.nr_nodes = 100;
-  args.nr_edges = 400;
+  add_vertex(g1);
   
-  boost::mt19937 rng(1);
-
-  graph g = generate_graph(args, rng);
-
-  vertex s = 0, t = 1;
-
-  boost::successive_shortest_path_nonnegative_weights(g, s, t);
-  int cost =  boost::find_flow_cost(g);
+  std::cout << num_vertices(g0) << std::endl;
+  std::cout << num_vertices(g1) << std::endl;
+  
+  edge e1, e2;
+  bool s1, s2;
+  
+  boost::tie(e1, s1) = boost::add_edge(0, 1, g0);
+  assert(s1);
+  
+  boost::tie(e2, s2) = boost::add_edge(0, 1, g1);
+  assert(s2);
 
   return 0;
 }
