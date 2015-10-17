84d25b69ecbdc1061db5a9714f45c9b5605be755
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-18 00:00:06 +0200

    try

--- try.cc
+++ try.cc
@@ -1,28 +1,40 @@
-#include<iostream>
+#include <map>
 
-#include "graph.hpp"
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
 
-int main() {
-  graph g0;
-  graph &g1 = g0.create_subgraph();
+int main()
+{
+  typedef
+    boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
+                          boost::no_property,
+                          boost::property<boost::edge_weight_t, int>>
+    graph;
 
-  add_vertex(g1);
-  add_vertex(g1);
-  
-  std::cout << num_vertices(g0) << std::endl;
-  std::cout << num_vertices(g1) << std::endl;
-  
-  edge e1, e2;
-  bool s1, s2;
-  
-  boost::tie(e1, s1) = boost::add_edge(0, 1, g0);
-  assert(s1);
-  
-  boost::tie(e2, s2) = boost::add_edge(0, 1, g1);
-  assert(s2);
+  typedef graph::edge_descriptor edge;
+  typedef graph::vertex_descriptor vertex;
 
-  std::cout << num_edges(g0) << std::endl;
-  std::cout << num_edges(g1) << std::endl;
-  
+  // Build the graph.
+  graph g;
+
+  // Fill in the maps.
+  std::map<edge, edge> e2e;
+  std::map<edge, int> e2c;
+  std::map<edge, int> e2r;
+  std::map<edge, int> e2w;
+
+  // Set the source and destination.
+  vertex s, t;
+
+  boost::associative_property_map<std::map<edge, edge>> rev(e2e);
+  boost::associative_property_map<std::map<edge, int>> cap(e2c);
+  boost::associative_property_map<std::map<edge, int>> res(e2r);
+  boost::associative_property_map<std::map<edge, int>> wgt(e2w);
+  boost::successive_shortest_path_nonnegative_weights(g, s, t,
+                                                      capacity_map(cap).
+                                                      residual_capacity_map(res).
+                                                      reverse_edge_map(rev).
+                                                      weight_map(wgt));
+
   return 0;
 }
