f3b34f7db4f39178a25b9326418b81e843ea37ae
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-19 10:35:53 +0200

    try

--- try.cc
+++ try.cc
@@ -17,14 +17,47 @@ int main()
   // Build the graph.
   graph g;
 
+  // Property map.
+  boost::property_map<graph, boost::edge_weight_t>::type
+    wm = get(boost::edge_weight_t(), g);
+
   // Fill in the maps.
   std::map<edge, edge> e2e;
   std::map<edge, int> e2c;
   std::map<edge, int> e2r;
   std::map<edge, int> e2w;
 
+  boost::graph_traits<graph>::edge_iterator ei, ee;
+  for (tie(ei, ee) = edges(g); ei != ee; ++ei)
+    {
+      // Normal edge.
+      const edge &ne = *ei;
+      // Reverse edge.
+      edge re;
+      // Status of the edge creation.
+      bool success;
+      boost::tie(re, success) =
+        boost::edge(boost::target(ne, g), boost::source(ne, g), g);
+      assert(success);
+
+      // Map the edges to each other.
+      e2e[ne] = re;
+      e2e[re] = ne;
+      
+      // The weight of the normal edge.
+      int weight = wm[ne];
+      
+      // Properties for the normal edge.
+      e2w[ne] = weight;
+      e2c[ne] = 1;
+      
+      // Properties for the reverse edge.
+      e2w[re] = -weight;
+      e2c[re] = 0;
+    }
+
   // Set the source and destination.
-  vertex s, t;
+  vertex s = 0, t = 1;
 
   boost::associative_property_map<std::map<edge, edge>> rev(e2e);
   boost::associative_property_map<std::map<edge, int>> cap(e2c);
