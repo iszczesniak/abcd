aa050b1e724157f241930f90a75b04aaa80321a4
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-19 13:18:24 +0200

    ksp

--- edge_disjoint_ksp.hpp
+++ edge_disjoint_ksp.hpp
@@ -1,59 +1,80 @@
-#ifndef EDGE_DISJOINT_KSP
-#define EDGE_DISJOINT_KSP
+#ifndef BOOST_GRAPH_EDGE_DISJOINT_KSP
+#define BOOST_GRAPH_EDGE_DISJOINT_KSP
 
-template <typename G>
-edge_disjoint_ksp(const G& g)
-{
-  // Property map.
-  boost::property_map<G, boost::edge_weight_t>::type
-    wm = get(boost::edge_weight_t(), g);
+#include <map>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
 
-  // Fill in the maps.
-  std::map<edge, edge> e2e;
-  std::map<edge, int> e2c;
-  std::map<edge, int> e2r;
-  std::map<edge, int> e2w;
+namespace boost {
 
-  boost::graph_traits<graph>::edge_iterator ei, ee;
-  for (tie(ei, ee) = edges(g); ei != ee; ++ei)
-    {
-      // Normal edge.
-      const edge &ne = *ei;
-      // Reverse edge.
-      edge re;
-      // Status of the edge creation.
-      bool success;
-      boost::tie(re, success) =
-        boost::edge(boost::target(ne, g), boost::source(ne, g), g);
-      assert(success);
+namespace detail {
 
-      // Map the edges to each other.
-      e2e[ne] = re;
+  template <typename Graph, class Weight>
+  void edge_disjoint_ksp(const Graph& g,
+                         typename graph_traits<Graph>::vertex_descriptor s,
+                         typename graph_traits<Graph>::vertex_descriptor t, 
+                         Weight w)
+  {
+    typedef typename graph_traits<Graph>::edge_descriptor edge;
+
+    // Fill in the maps.
+    std::map<edge, edge> e2e;
+    std::map<edge, int> e2c;
+    std::map<edge, int> e2r;
+    std::map<edge, int> e2w;
+
+    typename graph_traits<Graph>::edge_iterator ei, ee;
+    for (tie(ei, ee) = edges(g); ei != ee; ++ei)
+      {
+        // Normal edge.
+        const edge &ne = *ei;
+        // Reverse edge.
+        edge re;
+        // Status of the edge creation.
+        bool success;
+        boost::tie(re, success) =
+          boost::edge(boost::target(ne, g), boost::source(ne, g), g);
+        assert(success);
+
+        // Map the edges to each other.
+        e2e[ne] = re;
       
-      // The weight of the normal edge.
-      int weight = wm[ne];
+        // The weight of the normal edge.
+        int weight = wm[ne];
       
-      // Properties for the normal edge.
-      e2w[ne] = weight;
-      e2c[ne] = 1;
+        // Properties for the normal edge.
+        e2w[ne] = weight;
+        e2c[ne] = 1;
       
-      // Properties for the reverse edge.
-      e2w[re] = -weight;
-      e2c[re] = 0;
-    }
+        // Properties for the reverse edge.
+        e2w[re] = -weight;
+        e2c[re] = 0;
+      }
 
-  // Set the source and destination.
-  vertex s = 0, t = 1;
+    // Set the source and destination.
+    vertex s = 0, t = 1;
 
-  boost::associative_property_map<std::map<edge, edge>> rev(e2e);
-  boost::associative_property_map<std::map<edge, int>> cap(e2c);
-  boost::associative_property_map<std::map<edge, int>> res(e2r);
-  boost::associative_property_map<std::map<edge, int>> wgt(e2w);
-  boost::successive_shortest_path_nonnegative_weights(g, s, t,
-                                                      capacity_map(cap).
-                                                      residual_capacity_map(res).
-                                                      reverse_edge_map(rev).
-                                                      weight_map(wgt));
-}
+    boost::associative_property_map<std::map<edge, edge>> rev(e2e);
+    boost::associative_property_map<std::map<edge, int>> cap(e2c);
+    boost::associative_property_map<std::map<edge, int>> res(e2r);
+    boost::associative_property_map<std::map<edge, int>> wgt(e2w);
+    boost::successive_shortest_path_nonnegative_weights(g, s, t,
+                                                        capacity_map(cap).
+                                                        residual_capacity_map(res).
+                                                        reverse_edge_map(rev).
+                                                        weight_map(wgt));
+  }
 
-#endif /* EDGE_DISJOINT_KSP */
+}//detail
+
+  template <typename Graph, typename P, typename T, typename R>
+  void edge_disjoint_ksp(const Graph& g,
+                         typename graph_traits<Graph>::vertex_descriptor s,
+                         typename graph_traits<Graph>::vertex_descriptor t,
+                         const bgl_named_params<P, T, R>& params)
+  {
+    detail::edge_disjoint_ksp(g, s, t,
+                              choose_const_pmap(get_param(params, vertex_index),
+                                                g, vertex_index));
+  }
+
+#endif /* BOOST_GRAPH_EDGE_DISJOINT_KSP */
--- try.cc
+++ try.cc
@@ -1,72 +1,17 @@
-#include <map>
+#include "graph.hpp"
+#include "edge_disjoint_ksp.hpp"
 
-#include <boost/graph/adjacency_list.hpp>
+#include <map>
 #include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
 
 int main()
 {
-  typedef
-    boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
-                          boost::no_property,
-                          boost::property<boost::edge_weight_t, int>>
-    graph;
-
-  typedef graph::edge_descriptor edge;
-  typedef graph::vertex_descriptor vertex;
-
   // Build the graph.
   graph g;
 
-  // Property map.
-  boost::property_map<graph, boost::edge_weight_t>::type
-    wm = get(boost::edge_weight_t(), g);
-
-  // Fill in the maps.
-  std::map<edge, edge> e2e;
-  std::map<edge, int> e2c;
-  std::map<edge, int> e2r;
-  std::map<edge, int> e2w;
-
-  boost::graph_traits<graph>::edge_iterator ei, ee;
-  for (tie(ei, ee) = edges(g); ei != ee; ++ei)
-    {
-      // Normal edge.
-      const edge &ne = *ei;
-      // Reverse edge.
-      edge re;
-      // Status of the edge creation.
-      bool success;
-      boost::tie(re, success) =
-        boost::edge(boost::target(ne, g), boost::source(ne, g), g);
-      assert(success);
-
-      // Map the edges to each other.
-      e2e[ne] = re;
-      
-      // The weight of the normal edge.
-      int weight = wm[ne];
-      
-      // Properties for the normal edge.
-      e2w[ne] = weight;
-      e2c[ne] = 1;
-      
-      // Properties for the reverse edge.
-      e2w[re] = -weight;
-      e2c[re] = 0;
-    }
-
-  // Set the source and destination.
   vertex s = 0, t = 1;
 
-  boost::associative_property_map<std::map<edge, edge>> rev(e2e);
-  boost::associative_property_map<std::map<edge, int>> cap(e2c);
-  boost::associative_property_map<std::map<edge, int>> res(e2r);
-  boost::associative_property_map<std::map<edge, int>> wgt(e2w);
-  boost::successive_shortest_path_nonnegative_weights(g, s, t,
-                                                      capacity_map(cap).
-                                                      residual_capacity_map(res).
-                                                      reverse_edge_map(rev).
-                                                      weight_map(wgt));
+  boost::edge_disjoint_ksp(g, s, t);
 
   return 0;
 }
