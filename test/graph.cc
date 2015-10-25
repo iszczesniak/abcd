72f1de0717a202cbd654497f30fa24202a36c4c2
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-25 13:39:44 +0100

    graph

--- test/graph.cc
+++ test/graph.cc
@@ -29,20 +29,3 @@ BOOST_AUTO_TEST_CASE(parallel_edge_test)
   BOOST_CHECK(e2i[e2] == 2);
   BOOST_CHECK(e2i.size() == 2);
 }
-
-BOOST_AUTO_TEST_CASE(parallel_reverse_edge_test)
-{
-  graph g(2);
-  vertex a = *(vertices(g).first);
-  vertex b = *(vertices(g).first + 1);
-  edge e1, e2, e3;
-  bool s1, s2, s3;
-  tie(e1, s1) = add_edge(a, b, g);
-  tie(e2, s2) = add_edge(a, b, g);
-
-  // This is a free edge, i.e., it's not present in the graph.  We
-  // still want this descriptor to be different from others.
-  tie(e3, s3) = boost::edge(a, b, g);
-  BOOST_CHECK(e3 != e1);
-  BOOST_CHECK(e3 != e2);
-}
