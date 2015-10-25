87419c917ad6384d094feedc6f9507a80ba10791
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-10-25 15:53:58 +0100

    ksp

--- test/ksp.cc
+++ test/ksp.cc
@@ -28,24 +28,41 @@ aue(graph &g, vertex s, vertex d, int w)
   return make_pair(ade(g, s, d, w), ade(g, d, s, w));  
 }
 
-//      1
-//     /|\
-//    / | \
-//   0  |  3--4
-//    \ | /
-//     \|/
-//      2
+//       a
+//      /|\
+//     1 | 3 
+//    /  |  \
+//   b   2   c--5--d
+//    \  |  /
+//     4 | 6
+//      \|/
+//       e
 
 BOOST_AUTO_TEST_CASE(ksp_1)
 {
   graph g(5);
 
-  aue(g, 0, 1, 1);
-  aue(g, 0, 2, 1);
-  aue(g, 1, 2, 1);
-  aue(g, 1, 3, 1);
-  aue(g, 2, 3, 1);
-  aue(g, 3, 4, 1);
+  // Vertexes
+  vertex a = *(vertices(g).first + 0);
+  vertex b = *(vertices(g).first + 1);
+  vertex c = *(vertices(g).first + 2);
+  vertex d = *(vertices(g).first + 3);
+  vertex e = *(vertices(g).first + 4);
+
+  // Edges
+  edge ab, ba;
+  edge ae, ea;
+  edge ac, ca;
+  edge be, eb;
+  edge cd, dc;
+  edge ce, ec;
+
+  tie(ab, ba) = aue(g, a, b, 1);
+  tie(ae, ea) = aue(g, a, e, 2);
+  tie(ac, ca) = aue(g, a, c, 3);
+  tie(be, eb) = aue(g, b, e, 4);
+  tie(cd, dc) = aue(g, c, d, 5);
+  tie(ce, ec) = aue(g, c, e, 6);
 
   std::multimap<int, path> r;
 
