3b2a0d0ba6312acfc8be3568370facf1cadc8a45
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-11-18 22:14:49 +0100

    added the license statement

--- edge_disjoint_ksp.hpp
+++ edge_disjoint_ksp.hpp
@@ -1,3 +1,12 @@
+//=======================================================================
+// Copyright 2015 by Ireneusz Szcześniak
+// Authors: Ireneusz Szcześniak
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
 #ifndef BOOST_GRAPH_ED_KSP
 #define BOOST_GRAPH_ED_KSP
 
