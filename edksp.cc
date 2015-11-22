280c42d2c92bd4eda0a15c6d1bb9ed7c3c4464bb
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-11-22 08:25:39 +0100

    edksp respects the maximal length

--- edksp.cc
+++ edksp.cc
@@ -18,17 +18,20 @@ edksp::route_w(graph &g, const demand &d)
   paths = boost::edge_disjoint_ksp(g, s, t);
 
   for (auto const &p: paths)
-    {
-      // Find all the SSC along the path.
-      SSC ssc = find_path_ssc(g, p.second);
-      // Select the right SSC.
-      ssc = select_ssc(ssc, d.second);
-      // Candidate sscpath.
-      sscpath cp(p.second, ssc);
-      if (set_up_path(g, cp))
-        // The routing was successful, so return here.
-        return cp;
-    }
+    // Consider that path when there is no maximal length given or
+    // when the path lenght is not greater than the limit.
+    if (!m_ml || p.first <= m_ml.get())
+      {
+        // Find all the SSC along the path.
+        SSC ssc = find_path_ssc(g, p.second);
+        // Select the right SSC.
+        ssc = select_ssc(ssc, d.second);
+        // Candidate sscpath.
+        sscpath cp(p.second, ssc);
+        if (set_up_path(g, cp))
+          // The routing was successful, so return here.
+          return cp;
+      }
 
   // Return here in the case of failure.
   return sscpath();
