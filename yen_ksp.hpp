b44c32f4ade52970d91c0ed24ee0fe84f430efee
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-12-01 09:53:59 +0100

    yen

--- yen_ksp.hpp
+++ yen_ksp.hpp
@@ -1,10 +1,14 @@
 //=======================================================================
 // Copyright 2015 by Ireneusz Szcześniak
-// Authors: Ireneusz Szcześniak
+// Authors: Ireneusz Szcześniak <www.irkos.org>
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+//=======================================================================
+// https://en.wikipedia.org/wiki/Yen%27s_algorithm
 //=======================================================================
 
 #ifndef BOOST_GRAPH_YEN_KSP
@@ -12,6 +16,7 @@
 
 #include <list>
 #include <set>
+#include <multimap>
 
 #include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <boost/graph/filtered_graph.hpp>
@@ -21,17 +26,37 @@
  
 namespace boost {
 
-  // Exclude edge filter
+  // This filter excludes the given vertexes.
   template <typename Graph>
-  struct yenksp_filter
+  struct yenksp_vfilter
+  {
+    typedef typename Graph::vertex_descriptor vertex_descriptor;
+    typedef typename std::set<vertex_descriptor> vertex_set;
+
+    // The filter must be default-constructible, so it is.
+    yenksp_vfilter(): m_excluded() {};
+    
+    yenksp_vfilter(const vertex_set *excluded): m_excluded(excluded) {};
+
+    inline bool operator()(const vertex_descriptor &e) const
+    {
+      return m_excluded->count(e) == 0;
+    }
+
+    const vertex_set *m_excluded;
+  };
+
+  // This filter excludes the given edges.
+  template <typename Graph>
+  struct yenksp_efilter
   {
     typedef typename Graph::edge_descriptor edge_descriptor;
     typedef typename std::set<edge_descriptor> edge_set;
 
     // The filter must be default-constructible, so it is.
-    yenksp_filter(): m_excluded() {};
+    yenksp_efilter(): m_excluded() {};
     
-    yenksp_filter(const edge_set *excluded): m_excluded(excluded) {};
+    yenksp_efilter(const edge_set *excluded): m_excluded(excluded) {};
 
     inline bool operator()(const edge_descriptor &e) const
     {
@@ -54,16 +79,21 @@ namespace boost {
     typedef typename std::list<typename Graph::edge_descriptor> path_type;
     typedef typename Weight::value_type weight_type;
 
-    // The result.
-    std::list<std::pair<weight_type, path_type>> result;
+    // The results.
+    std::list<std::pair<weight_type, path_type>> A;
+    // The tentative results.
+    std::multimap<std::pair<weight_type, path_type>> B;
 
-    // The set of excluded vertexes.
-    std::set<vertex_descriptor> exv;
     // The set of excluded edges.
     std::set<edge_descriptor> exe;
-    
-    // The filter for excluding vertexes and edges.
-    edksp_filter<Graph> f(&exv, &exe);
+    // The set of excluded vertexes.
+    std::set<vertex_descriptor> exv;
+   
+    // The filter which excludes edges.
+    yenksp_efilter<Graph> f(&exe);
+    // The filter which excludes vertexes.
+    yenksp_vfilter<Graph> f(&exv);
+
     // The filtered graph type.
     typedef boost::filtered_graph<Graph, yenksp_filter<Graph> > fg_type;
     // The filtered graph.
@@ -123,3 +153,52 @@ namespace boost {
 } // boost
 
 #endif /* BOOST_GRAPH_YEN_KSP */
+
+function YenKSP(Graph, source, sink, K):
+   // Determine the shortest path from the source to the sink.
+   A[0] = Dijkstra(Graph, source, sink);
+   // Initialize the heap to store the potential kth shortest path.
+   B = [];
+   
+   for k from 1 to K:
+       // The spur node ranges from the first node to the next to last node in the previous k-shortest path.
+       for i from 0 to size(A[k − 1]) − 1:
+           
+           // Spur node is retrieved from the previous k-shortest path, k − 1.
+           spurNode = A[k-1].node(i);
+           // The sequence of nodes from the source to the spur node of the previous k-shortest path.
+           rootPath = A[k-1].nodes(0, i);
+           
+           for each path p in A:
+               if rootPath == p.nodes(0, i):
+                   // Remove the links that are part of the previous shortest paths which share the same root path.
+                   remove p.edge(i, i + 1) from Graph;
+           
+           for each node rootPathNode in rootPath except spurNode:
+               remove rootPathNode from Graph;
+           
+           // Calculate the spur path from the spur node to the sink.
+           spurPath = Dijkstra(Graph, spurNode, sink);
+           
+           // Entire path is made up of the root path and spur path.
+           totalPath = rootPath + spurPath;
+           // Add the potential k-shortest path to the heap.
+           B.append(totalPath);
+           
+           // Add back the edges and nodes that were removed from the graph.
+           restore edges to Graph;
+           restore nodes in rootPath to Graph;
+                   
+       if B is empty:
+           // This handles the case of there being no spur paths, or no spur paths left.
+           // This could happen if the spur paths have already been exhausted (added to A), 
+           // or there are no spur paths at all - such as when both the source and sink vertices 
+           // lie along a "dead end".
+           break;
+       // Sort the potential k-shortest paths by cost.
+       B.sort();
+       // Add the lowest cost path becomes the k-shortest path.
+       A[k] = B[0];
+       B.pop();
+   
+   return A;
