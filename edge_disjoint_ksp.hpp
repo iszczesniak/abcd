abf02dc444f2810ef1d9a3a39312faa209491fbd
Irek Szcześniak <irek.szczesniak@gmail.com>
2015-12-02 12:02:05 +0100

    added some comments and made the dijkstra call custom

--- edge_disjoint_ksp.hpp
+++ edge_disjoint_ksp.hpp
@@ -7,6 +7,15 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
 
+//=======================================================================
+// Function edge_disjoint_ksp finds the k-shortest paths which are
+// edge-disjoint, i.e. the paths do not have common edges.  The graph
+// can have parallel edges.  The algorithm employs the Dijkstra
+// algorithm to find the shortest path, then its edges are disabled,
+// and Dijkstra finds the second shortest path, and so on until k
+// paths are found or there are no more paths.
+//=======================================================================
+
 #ifndef BOOST_GRAPH_EDGE_DISJOINT_KSP
 #define BOOST_GRAPH_EDGE_DISJOINT_KSP
 
@@ -16,9 +25,10 @@
 #include <boost/graph/dijkstra_shortest_paths.hpp>
 #include <boost/graph/filtered_graph.hpp>
 #include <boost/graph/visitors.hpp>
+#include <boost/optional.hpp>   
 #include <boost/property_map/vector_property_map.hpp>
 #include <boost/utility/value_init.hpp>
- 
+
 namespace boost {
 
   // Exclude edge filter
@@ -42,6 +52,55 @@ namespace boost {
   };
 
   template <typename Graph, typename Weight>
+  optional<std::pair<typename Weight::value_type,
+                     std::list<typename Graph::edge_descriptor>>>
+  custom_dijkstra_call(const Graph &g,
+                       typename graph_traits<Graph>::vertex_descriptor s,
+                       typename graph_traits<Graph>::vertex_descriptor t,
+                       Weight wm)
+  {
+    typedef typename graph_traits<Graph>::vertex_descriptor vertex_descriptor;
+    typedef typename graph_traits<Graph>::edge_descriptor edge_descriptor;
+    typedef typename std::list<typename Graph::edge_descriptor> path_type;
+    typedef typename Weight::value_type weight_type;
+    typedef typename std::pair<weight_type, path_type> kr_type;
+    
+    vector_property_map<edge_descriptor> pred(num_vertices(g));
+  
+    dijkstra_shortest_paths
+      (g, s, weight_map(wm).
+       visitor(make_dijkstra_visitor(record_edge_predecessors(pred, on_edge_relaxed()))));
+
+    optional<kr_type> result;
+
+    // Was the solution found?
+    if (pred[t] != edge_descriptor())
+      {
+        // The cost of the shortest path.
+        value_initialized<weight_type> cost;
+        // The path found.
+        path_type path;
+
+        // Trace the solution to the source.
+        vertex_descriptor c = t;
+        while (c != s)
+          {
+            const edge_descriptor &e = pred[c];
+            // Build the path.
+            path.push_front(e);
+            // Calculate the cost of the path.
+            cost += get(wm, e);
+            // Find the predecessing vertex.
+            c = source(e, g);
+          }
+
+        result = std::make_pair(cost, path);
+      }
+
+    return result;
+  }
+  
+  template <typename Graph, typename Weight>
   std::list<std::pair<typename Weight::value_type,
                       std::list<typename Graph::edge_descriptor>>>
   edge_disjoint_ksp(const Graph& g,
@@ -53,6 +112,7 @@ namespace boost {
     typedef typename graph_traits<Graph>::edge_descriptor edge_descriptor;
     typedef typename std::list<typename Graph::edge_descriptor> path_type;
     typedef typename Weight::value_type weight_type;
+    typedef typename std::pair<weight_type, path_type> kr_type;
 
     // The result.
     std::list<std::pair<weight_type, path_type>> result;
@@ -69,39 +129,20 @@ namespace boost {
     // In each iteration, we try to find a shortest path.
     do
       {
-        vector_property_map<edge_descriptor> pred(num_vertices(g));
+        // This is the optional k-th result.
+        optional<kr_type> okr = custom_dijkstra_call(g, s, t, wm);
 
-        dijkstra_shortest_paths
-          (fg, s,
-           weight_map(wm).
-           visitor(make_dijkstra_visitor(record_edge_predecessors(pred, on_edge_relaxed()))));
-
-        // Break the loop if no solution was found.
-        if (pred[t] == edge_descriptor())
+        if (!okr)
           break;
 
-        // The cost of the shortest path.
-        value_initialized<weight_type> cost;
-        // The path found.
-        path_type path;
+        // This is the k-th result.
+        const kr_type &kr = okr.get();
+        
+        // Exclude the edges from the k-th result found.
+        for(auto const &e: kr.second)
+          excluded.insert(e);
 
-        // Trace the solution to the source.
-        vertex_descriptor c = t;
-        while (c != s)
-          {
-            const edge_descriptor &e = pred[c];
-            // Build the path.
-            path.push_front(e);
-            // Exclude the edge, so that it's not used in the next
-            // shortest paths.
-            excluded.insert(e);
-            // Calculate the cost of the path.
-            cost += get(wm, e);
-            // Find the predecessing vertex.
-            c = source(e, g);
-          }
-
-        result.push_back(std::make_pair(cost, path));
+        result.push_back(kr);
 
       } while(true);
       
