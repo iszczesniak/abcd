e74afe87e21717148861c36cda02b0457cdf32a8
Irek Szcześniak <irek.szczesniak@gmail.com>
2016-01-05 11:05:52 +0100

    reverse

--- reverse.cc
+++ reverse.cc
@@ -11,6 +11,10 @@ main()
 
   m[1] = "one";
 
+  for(auto r = m.rbegin(); r != m.rend();)
+    if ((r++)->first == 1)
+      m.erase(r.base());
+  
   for(auto const &e: m)
     cout << e.first << " -> " << e.second << endl;
 }
