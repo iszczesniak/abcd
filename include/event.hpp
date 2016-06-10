68e60b92e7ec43b2bcaadccba4d11acd0f69c04a
Irek Szcześniak <irek@irkos.org>
2016-06-10 09:58:19 +0200

    moved the code to the header file

--- include/event.hpp
+++ include/event.hpp
@@ -1,23 +1,33 @@
 #ifndef EVENT_HPP
 #define EVENT_HPP
 
-#include <queue>
-
-class module;
+#include "module.hpp"
 
 template<typename S>
-class event
+class event: public S
 {
-  S::time_type t;
-  module<S> *m;
+  S::time_type m_t;
+  module<S> *m_m;
 
 public:
-  event(double t, module *m);
-  void process() const;
-  bool operator<(const event<S> &e) const;
-  double get_time() const;
-};
+  event(S::time_type t, module<S> *m): m_t(t), m_m(m)
+  {
+  }
 
-typedef std::priority_queue<event> pqueue;
+  void process() const
+  {
+    (*m_m)(m_t);
+  }
+
+  bool operator<(const event<S> &e) const
+  {
+    return m_t > e.m_t;
+  }
+
+  S::time_type get_time() const
+  {
+    return m_t;
+  }
+};
 
 #endif /* EVENT_HPP */
